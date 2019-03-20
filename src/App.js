897f7fbc42b384e09f9993211ae0429579093a7d
Dan Alvares <dm.alvares@gmail.com>
2019-03-20 14:26:35 +0000

    Created Movie components, with mock data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header'
+import Movies from './components/Movies/Movies';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Movies />
       </div>
     );
   }
--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Movie = () => {
+const Movie = (props) => {
+    const [movie, setViewing] = useState(props.movie)
+
     return ( 
-        <section class="movie">
+        <section className="Movie" onClick={ () => setViewing({...movie, viewing: movie.viewing = !movie.viewing }) }>
+                
+            <h2>{ movie.title }<span className="badge badge-info">7.4</span></h2>
+            <img className={ movie.viewing ? 'viewing' : ''} src={ '//image.tmdb.org/t/p/w300/' + movie.poster_path } alt="{ props.movie.title }" />
+            <div className="body-text">
+                <img  src={ '//image.tmdb.org/t/p/w154/' + movie.poster_path } width="70" alt="{ props.movie.title }" />
+                <p>{ movie.overview }</p>
+                <div className="genres"> 
+                    {
+                        movie.genres.map(genre => <span className="badge">{genre}</span>)
+                    }
+                </div>
+            </div>
         </section> 
     );
 }
