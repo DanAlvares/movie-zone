9f76cbb910b3849358fd7920abbada33b92300fb
Dan Alvares <dm.alvares@gmail.com>
2019-03-20 14:29:21 +0000

    Remove reference to service worker

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// For the app to work offline and load faster
-// change unregister() to register() below. 
-serviceWorker.unregister();
